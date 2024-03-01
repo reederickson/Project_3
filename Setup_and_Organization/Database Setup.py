4ee2a3afd12253c34f298e65c6f9cce85ba92efb
bryanpijohnson <47281061+bryanpijohnson@users.noreply.github.com>
2024-02-29 19:25:29 -0500

    had to revert database setup back to what it was before

--- Setup_and_Organization/Database Setup.py
+++ Setup_and_Organization/Database Setup.py
@@ -81,14 +81,14 @@ gender_list = list(df["Gender"].unique())
 gender_dict = {"id": [i for i in range(len(gender_list))], "gender_name": [i for i in gender_list]}
 gender = pd.DataFrame(gender_dict)
 gender.to_csv("../Resources/Database_CSVs/gender.csv", index = False)
-gender.to_sql(name = "gender", con = engine, if_exists= "fail")
+gender.to_sql(name = "gender", con = engine, if_exists= "replace")
 
 ## Creating the occupation table csv and table in SQL
 occupation_list = list(df["Occupation"].unique())
 occupation_dict = {"id": [i for i in range(len(occupation_list))], "occupation_name": [i for i in occupation_list]}
 occupation = pd.DataFrame(occupation_dict)
 occupation.to_csv("../Resources/Database_CSVs/occupation.csv", index = False)
-occupation.to_sql(name = "occupation", con = engine, if_exists = "fail")
+occupation.to_sql(name = "occupation", con = engine, if_exists = "replace")
 
 ## Creating the person table csv and table in SQL
 person_gender_occupation = person.merge(gender, left_on = "Gender",right_on = "gender_name", how = "inner")\
@@ -96,7 +96,7 @@ person_gender_occupation = person.merge(gender, left_on = "Gender",right_on = "g
 person_cleaned = person_gender_occupation[["Person ID", "id_x", "Age", "id_y"]]\
     .rename(columns = {"Person ID": "id", "id_x": "gender_id", "Age": "age", "id_y": "occupation_id"})
 person_cleaned.to_csv("../Resources/Database_CSVs/person.csv", index = False)
-person_cleaned.to_sql(name = "person", con = engine, if_exists= "fail")
+person_cleaned.to_sql(name = "person", con = engine, if_exists= "replace")
 
 ## Creating the sleep_disorder table csv and table in SQL
 df["Sleep Disorder"].fillna(value = "None", axis = 0, inplace = True)
@@ -104,7 +104,7 @@ sleep_disorder_list = list(df["Sleep Disorder"].unique())
 sleep_disorder_dict = {"id": [i for i in range(len(sleep_disorder_list))], "sleep_disorder_name": [i for i in sleep_disorder_list]}
 sleep_disorder = pd.DataFrame(sleep_disorder_dict)
 sleep_disorder.to_csv("../Resources/Database_CSVs/sleep_disorder.csv", index = False)
-sleep_disorder.to_sql(name = "sleep_disorder", con = engine, if_exists = "fail")
+sleep_disorder.to_sql(name = "sleep_disorder", con = engine, if_exists = "replace")
 
 ## Creating the sleep table csv and table in SQL
 sleep_df = df[["Person ID", "Sleep Duration", "Quality of Sleep", "Sleep Disorder"]]
@@ -112,13 +112,13 @@ sleep_cleaned = sleep_df.merge(sleep_disorder, left_on = "Sleep Disorder", right
 sleep = sleep_cleaned[["Person ID", "Sleep Duration", "Quality of Sleep", "id"]].reset_index()\
     .rename(columns = {"index": "id", "Person ID": "person_id", "Sleep Duration": "sleep_duration", "Quality of Sleep": "sleep_quality", "id": "sleep_disorder_id"})
 sleep.to_csv("../Resources/Database_CSVs/sleep.csv", index = False)
-sleep.to_sql(name = "sleep", con = engine, if_exists = "fail")
+sleep.to_sql(name = "sleep", con = engine, if_exists = "replace")
 
 ## Creating the activity table csv and table in SQL
 activity_df = df[["Person ID", "Physical Activity Level", "Daily Steps"]].reset_index()\
     .rename(columns = {"index": "id", "Person ID": "person_id", "Physical Activity Level": "activity_level", "Daily Steps": "daily_steps"})
 activity_df.to_csv("../Resources/Database_CSVs/activity.csv", index = False)
-activity_df.to_sql(name = "activity", con = engine, if_exists = "fail")
+activity_df.to_sql(name = "activity", con = engine, if_exists = "replace")
 
 ## Creating the bmi table csv and table in SQL
 df["BMI Category"] = np.where(df["BMI Category"] == "Normal Weight", "Normal", df["BMI Category"])
@@ -126,7 +126,7 @@ bmi_list = list(df["BMI Category"].unique())
 bmi_dict = {"id": [i for i in range(len(bmi_list))], "bmi_name": [i for i in bmi_list]}
 bmi_df = pd.DataFrame(bmi_dict)
 bmi_df.to_csv("../Resources/Database_CSVs/bmi.csv", index = False)
-bmi_df.to_sql(name = "bmi", con = engine, if_exists = "fail")
+bmi_df.to_sql(name = "bmi", con = engine, if_exists = "replace")
 
 ## Creating the health table csv and table in SQL
 df[["Systolic", "Diastolic"]] = df["Blood Pressure"].str.split("/", expand = True)
@@ -144,7 +144,7 @@ health = health_cleaned[["Person ID", "Blood Pressure", "Stress Level", "Heart R
                        "id": "bmi_id"
                        })    
 health.to_csv("../Resources/Database_CSVs/health.csv", index = False)
-health.to_sql(name = "health", con = engine, if_exists = "fail")
+health.to_sql(name = "health", con = engine, if_exists = "replace")
 
 ## Closing the connection to the database
 engine.dispose()
